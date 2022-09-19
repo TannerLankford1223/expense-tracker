16532eebbd4c6ed3da6f696b5059e978246329db
TannerLankford1223 <T.Lankford730@gmail.com>
2022-09-19 16:45:11 -0500

    Remove default elements

--- src/App.js
+++ src/App.js
@@ -5,11 +5,7 @@ import ExpenseItem from './components/ExpenseItem'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-          <h2>Let's get started!</h2>
           <ExpenseItem></ExpenseItem>
-      </header>
     </div>
   );
 }
